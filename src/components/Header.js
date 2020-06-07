a40ee28472442c3d8583b7bc158e3bb56e31de7a
Gavin <gavin.lui@gmail.com>
2020-06-07 23:09:14 +1000

    fixed logo sizing and added header background

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-import { Toolbar } from "@material-ui/core";
-import logo from "../images/Whisk_and_bake_logo.jpeg";
+import { CardMedia, Toolbar } from "@material-ui/core";
+import logo from "../images/wnb_logo_crop.png";
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <img src={logo}></img>
+        <div className="wnb-header-logo">
+          <img width="200px" src={logo}></img>
+        </div>
         <AppBar position="static" className="navbar" color="primary">
           <Toolbar>
             <Button>ABOUT</Button>
